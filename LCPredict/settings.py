ebb2765149410510448e0270dfb23d30cdbb37a2
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2021-06-05 01:31:11 +0530

    DB changes

--- LCPredict/settings.py
+++ LCPredict/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY =  os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
